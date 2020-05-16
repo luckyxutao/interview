252c5fbb65b0bc6f3a10e7aaa3c8e6d0a144d2f6
luckycuiying <331502715@qq.com>
2020-05-16 15:06:51 +0800

    upd

--- src/v2/lowest-common-ancestor-of-a-binary-search-tree.js
+++ src/v2/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -15,6 +15,33 @@ var lowestCommonAncestor = function (root, p, q) {
     if (!root) {
         return null;
     }
+    let queue = [root];
+    let cur;
+    while(queue.length){
+        cur = queue.pop();
+        console.log(cur.val);
+        //根据BST的性质，小 中 大 分布，一旦遇到不是在一边情况，则直接返回
+        if(p.val > cur.val && q.val > cur.val){
+            cur.right && queue.unshift(cur.right);
+        } else if( p.val < cur.val && q.val < cur.val ){
+            cur.left && queue.unshift(cur.left);
+        } else {
+            return cur;
+        }
+    }
+    return root;
+};
+const { TreeNode } = require('../lib/TreeNode')
+const constructTreeFromArray = require('../lib/constructTreeFromArray');
+// let root2 = constructTreeFromArray([1, 2, 3, 4, 5]);
+let root2 = constructTreeFromArray([6, 2, 8, 0, 4, 7, 9, null, null, 3, 5]);
+console.log(lowestCommonAncestor(root2, new TreeNode(2), new TreeNode(8)));
+
+
+var lowestCommonAncestorWithRecursion = function (root, p, q) {
+    if (!root) {
+        return null;
+    }
     //如果都大于root.val，说明元素位于树的右边
     if (p.val > root.val && q.val > root.val) {
         return lowestCommonAncestor(root.right,p,q);
@@ -24,8 +51,3 @@ var lowestCommonAncestor = function (root, p, q) {
     }
     return root;
 };
-const { TreeNode } = require('../lib/TreeNode')
-const constructTreeFromArray = require('../lib/constructTreeFromArray');
-// let root2 = constructTreeFromArray([1, 2, 3, 4, 5]);
-let root2 = constructTreeFromArray([6, 2, 8, 0, 4, 7, 9, null, null, 3, 5]);
-console.log(lowestCommonAncestor(root2, new TreeNode(2), new TreeNode(4)));
