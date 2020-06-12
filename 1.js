12361eaa8151c3dd246083c4c591f56cf99f2939
luckycuiying <331502715@qq.com>
2020-06-12 10:46:14 +0800

    upd

--- 1.js
+++ 1.js
@@ -1,35 +1,74 @@
-// // const path = require('path');
-// // const { join,dirname} = path.posix;//保证不同操作系统唯一性，不同系统分割符不一样
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findNthDigit = function (n) {
+    /*
+        [0,1,2,3,4,5,6,7,8,9]
+        1. 1 * 9 = 9
+           2 * 90 = 180
+           3 * 900 = 2700
+           ...
+    */
+    //[0-9]
+    if (n < 10) {
+        return n;
+    }
 
-// // var a = './src' + './hello.js'
-// // console.log(join( './src' ,'./hello.js'));
+    let numWidth = 2;
+    const baseN = 9;
+    let numSum = 9;
+    let segmentStart = 10;
+
+    while (true) {
+        //189 = 9 + 10*9*2
+        // 189 + 100* 9 * 3
+        //两位才是从10-99 
+        segmentStart = Math.pow(10, numWidth - 1);
+        let v2 = segmentStart * baseN * numWidth;
+        numSum = numSum + v2
+        if (numSum > n) {
+            numSum -= v2;
+            break;
+        }
+        numWidth++;//2
+
+    }
+    /*
+        nmWidth= 2
+        segmentStart = 10(含)
+        365-189
+    */
+    let diff = n - numSum;//176
+    let target = (segmentStart - 1) + Math.ceil(diff / numWidth);
+    //确定 在target上的位置
+
+    let mod = diff % numWidth;
+    target = target + ''
+    if (mod === 0) {
+        //则是最后一个元素
+        return target.charAt(target.length - 1);
+    } else {
+        /*
+            1,2,3  4,5,6  7,8,9
+            8  8/3 = 3 = [7,8,9]
+            8 % 3 = 2
+        */
+        return target.charAt(mod - 1);
+    }
+};
 
 
-// // var aa = new Map();
-// // var s = {};
-// // var s2 = {};
-// // aa.set(s,1);
-// // aa.set(s2,1);
-// // // console.log(aa.)
-// // console.log(aa.size)
-// // var f = {
-// //     a: function () {
-
-// //     },
-// //     d: [1, 2, 3],
-// //     b: new Date(),
-// //     c: new RegExp()
-// // }
-
-// var arr = [1,2,3,4];
-// let date = new Date();
-// var inst = new arr.constructor();
-// for(let ele in arr){
-//     inst[ele] = arr[ele]
-// }
-// console.log(inst);
+console.log(findNthDigit(365))
 
 
-// // console.log(JSON.stringify(f));
-var obj = Object.create(null);
-console.log(obj.hasOwnProperty('a'));
+
+
+
+
+
+
+
+
+
+
