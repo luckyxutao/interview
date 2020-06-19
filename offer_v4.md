c65b56248f7cf63aa63f57b04277f12e6263791e
luckycuiying <331502715@qq.com>
2020-06-20 07:57:49 +0800

    upd

--- offer_v4.md
+++ offer_v4.md
@@ -5,7 +5,7 @@
 * [2. 两数相加](https://leetcode-cn.com/problems/add-two-numbers/)
 [解析](https://leetcode-cn.com/problems/add-two-numbers/solution/2-liang-shu-xiang-jia-chu-deng-shu-xue-by-luckyxut/)
 * [14. 最长公共前缀](https://leetcode-cn.com/problems/longest-common-prefix/)
-[解析](https://leetcode-cn.com/problems/longest-common-prefix/solution/14-zui-chang-gong-gong-qian-zhui-by-luckyxutao/)
+[解析](https://leetcode-cn.com/problems/longest-common-prefix/solution/14-zui-chang-gong-gong-qian-zhui-zhu-lie-dui-bi-by/)
 
 ### 二分
 * [704. 二分查找](https://leetcode-cn.com/problems/binary-search/)
--- src/leetcode/v4/multipy.js
+++ src/leetcode/v4/multipy.js
@@ -3,33 +3,36 @@
  * @param {string} num2
  * @return {string}
  */
-var multiply = function (num1, num2) {
-    let m1Len = num1.length, m2Len = num2.length;
-    let res = [];
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
     /*
-        0   1   2     1  2   3
-            0   1        4   5
+        1 2 3
+          4 5
+
+          2
+          3
     */
-    for (let i = m1Len - 1; i >= 0; i--) {
-        for (let j = m2Len - 1; j >= 0; j--) {
-            let mul = num1.charAt(i) * num2.charAt(j);
-            let p1 = i + j, p2 = i + j + 1;
-            /*
-                8  0
-                7
-                5
-            */
-            let sum = mul + (res[p2] || 0);
+    let res = [];
+    for(let i = num1.length-1;i>=0;i--){
+        for(let j = num2.length-1;j>=0;j--){
+            let p1 = i+j, p2 = i+j+1;
+            let sum = (res[p2]||0) + num1.charAt(i) * num2.charAt(j);
             res[p2] = sum % 10;
-            res[p1] = Math.floor(sum / 10) + (res[p1] || 0);
+            res[p1] = (res[p1]||0) + Math.floor(sum / 10);
         }
     }
-    let i = 0;
-    while (res.length && res[0] === 0) {
+    /*
+    0 5 5 5
+    */
+    while(res.length && res[0]=== 0){
         res.shift();
     }
-    return i == res.length ? '0' : res.join('')
+    return res.join('');
 };
-multiply('123', '45')
+multiply('2', '3')
 // 1  2   3
 //    4   5
